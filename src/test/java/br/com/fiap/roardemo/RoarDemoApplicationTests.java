7373063636317c5e309020429b4c89d661de1049
Sabrina <sabis476@gmail.com>
2025-04-08 21:54:17 -0300

    Inicio da autenticação

--- src/test/java/br/com/fiap/roardemo/RoarDemoApplicationTests.java
+++ src/test/java/br/com/fiap/roardemo/RoarDemoApplicationTests.java
@@ -1,13 +1,13 @@
-package br.com.fiap.roardemo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RoarDemoApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package br.com.fiap.roardemo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class RoarDemoApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
